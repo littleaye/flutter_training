329e01d64b63d91d5b19f642913f26a1c8c7c2f6
Korakoch P <korakoch.pr@chococrm.com>
2024-01-09 17:33:15 +0700

    [Update] Create pizza counter to test Bloc and model flutter

---
+++ lib/models/pizza_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
+
+@override
+class Pizza extends Equatable {
+  final String id;
+  final String name;
+  final Image image;
+
+  const Pizza({required this.id, required this.name, required this.image});
+
+  @override
+  List<Object?> get props => [id, name, image];
+
+  static List<Pizza> pizzas = [
+    Pizza(id: '0', name: 'Pizza', image: Image.asset('images/pizza.png')),
+    Pizza(
+        id: '1',
+        name: 'Pizza Pepperoni',
+        image: Image.asset('images/pizza_pepperoni.png'))
+  ];
+}
