598165de5cfb6a5c05a9fa33244c834d3498ec49
Korakoch P <korakoch.pr@chococrm.com>
2024-01-17 14:05:37 +0700

    [Update] set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: "AIzaSyA-mKukUySWeEPN_zAhnVLr1rBOKbfRI1s",
-      appId: "",
-      messagingSenderId: "643072333337",
-      projectId: "authentication-9c2f7",
-    ),
+        apiKey: "AIzaSyA-mKukUySWeEPN_zAhnVLr1rBOKbfRI1s",
+        authDomain: "authentication-9c2f7.firebaseapp.com",
+        projectId: "authentication-9c2f7",
+        storageBucket: "authentication-9c2f7.appspot.com",
+        messagingSenderId: "643072333337",
+        appId: "1:643072333337:web:b67878cc33f99039aefc88",
+        measurementId: "G-NEZ1KRD6MS"),
   );
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
