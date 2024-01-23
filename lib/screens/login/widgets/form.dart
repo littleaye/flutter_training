acde2bbf2b484ce4340d99322af0fb7f91cce92e
KORAKOCH PRATHUMMA <korakoch.pr@chococrm.com>
2024-01-23 14:12:19 +0700

    [update] set up homepage

--- lib/screens/login/widgets/form.dart
+++ lib/screens/login/widgets/form.dart
@@ -1,12 +1,18 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'login_button.dart';
 import 'password_field.dart';
 import 'username_field.dart';
 
-class FormWidget extends StatelessWidget {
+class FormWidget extends StatefulWidget {
   const FormWidget({super.key});
 
+  @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -16,7 +22,7 @@ class FormWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: [
-           const UsernameField(),
+          const UsernameField(),
           const PasswordField(),
           const SizedBox(height: 15),
           LoginButton(formKey: _formKey),
